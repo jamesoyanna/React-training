72df5c50cba36ee132a10c8a9f8865593485d8ff
jamesoyanna <engrjayt200@gmail.com>
2023-09-09 14:21:44 +0100

    React Fundamentals

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './button.css';
+
+const Button  = () => {
+
+    return (
+        <div>
+            <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out'> Apply Now</button>
+        </div>
+    )
+
+}
+export default Button;
